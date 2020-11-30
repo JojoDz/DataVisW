191d68e2f1226e6b7aadc55b544c12c7ffad699e
nonoleroux <72551268+nonoleroux@users.noreply.github.com>
2020-11-30 17:18:09 +0100

    add slogan display, img positions, randomize armAppear

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
 //changer la valeur de display #slogan
-let sloganTime = document.getElementById("slogan");
-
-function sloganDisplay(){
-    sloganTime.style.display = "block";
-    //console.log("je suis dans sloganDisplay");
-}
-setTimeout(sloganDisplay, 5000);
+// let sloganTime = document.getElementById("slogan");
+//
+// function sloganDisplay(){
+//     sloganTime.style.display = "block";
+//     //console.log("je suis dans sloganDisplay");
+// }
+// setTimeout(sloganDisplay, 2000);
 //console.log("ici setTimeout");
 
 //apparition des bras dans la div #armAppear
 let armAppear = document.getElementById("armAppear");
+function randomMax(max){
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 function armDisplay(){
-    armAppear.innerHTML +='<img src="bras.svg" style="margin-left:'+ Math.random(7000) +'"/>'
+    armAppear.innerHTML += '<img src="bras.svg" style="margin-left:'+ randomMax(50) +'px"/>'
 }
+setTimeout(armDisplay,3500);
 setInterval(armDisplay, 1000);
